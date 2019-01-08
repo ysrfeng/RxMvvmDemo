b533c5fc763729f0711dd668e77b4c61e607bc6c
ysr_feng <yfeng1023@gmail.com>
2019-01-08 09:40:33 +0800

    脚本配置

--- app/src/main/java/com/feng/rx/MainActivity.java
+++ app/src/main/java/com/feng/rx/MainActivity.java
@@ -3,7 +3,14 @@ package com.feng.rx;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+/**
+ * Package Name: com.feng.rx
+ *
+ * @author ysr
+ * @Email yfeng1023@gmail.com
+ * @Description: TODO
+ * @Create Date: 2019/1/7
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
